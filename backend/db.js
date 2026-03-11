12ca9a05fc7cc83f0bfd099bbd475acbf15adff6
manyaa08 <codemewith4@gmail.com>
2026-03-12 00:18:24 +0530

    Move DB credentials to environment variables

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "mk082406",
-    database: "Mediconnect"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-db.connect((err)=>{
-    if(err){
+db.connect((err) => {
+    if (err) {
         console.log("DB Error", err);
     } else {
         console.log("MySQL Connected");
